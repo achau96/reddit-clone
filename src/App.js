d7eeb1b0d04b74622cc030019dee1621a5589950
Austin Chau <austin.chau1@gmail.com>
2021-11-19 20:08:33 -0500

    useRef to create searchbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import './styles.css';
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import searchIcon from './black-24dp/1x/outline_search_black_24dp.png';
 
 const Navbar = (props) => {
-  return <div>Hello</div>;
+  const textInput = useRef(null);
+
+  const handleClick = () => {
+    textInput.current.focus();
+  };
+
+  return (
+    <div className="navbar">
+      <div>Logo</div>
+      <div className="flex searchBar">
+        <div className="icon" onClick={handleClick}>
+          <img src={searchIcon} alt="search icon" />
+        </div>
+        <div>
+          <input className="searchInput" type="text" ref={textInput}></input>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="login">Login</div>
+        <div className="signup">Signup</div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
